9158cea8262aadaaa448db0fcca876627a47fb4c
Vishnvardhan <71860406+vishnuvardhan015@users.noreply.github.com>
2024-05-17 14:43:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const App = () => {
+  useState();
+  return (
+    <section>
+      <h1 className="omen">"Hello World"</h1>
+    </section>
+  );
+};
+
+export default App;
